d6b21f1a65e73a4d9c381189fd6929da48ce6b3e
DzifaAgbenyoh <43419391+DzifaAgbenyoh@users.noreply.github.com>
2018-11-09 14:30:58 -0500

    updated

--- CompanyLogoAnimationDzifa/main.lua
+++ CompanyLogoAnimationDzifa/main.lua
@@ -1,7 +1,12 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Title: Company Logo Animation
+-- Name: Dzifa
+-- Course: ICS2O/3C
+-- This program animates the company logo on the screen
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--Make local function
+local vampire = display.newImageRect("Images/vampire.png", 300, 200)
